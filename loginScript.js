db9efc2df6c65a0f0957e9437898f074daa036fb
SriramMas <78671890+SriramMas@users.noreply.github.com>
2021-02-09 17:43:51 +1100

    Update loginScript.js

--- loginScript.js
+++ loginScript.js
@@ -1,3 +1,9 @@
 $('#loginButton').on('submit',function(){
-	var password=$('$passInput').val()
+	var password=$('passInput').val();
+	var linkToLog='loggedIn';
+	if(password==='rocketx29'){
+		linkToLog+='5kqmgfnd3avxj5d4';
+	}else{
+		linkToLog+='None';
+	}
 });
